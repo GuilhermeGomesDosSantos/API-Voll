b171c84f3f4dc5737db78def3cf88d130bbdf4f3
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-07-15 00:08:40 -0300

    Atualizando dados

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -91,4 +91,18 @@ public class Medico {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedicos dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
